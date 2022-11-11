17fe8ace2bd0dd5e1d1449302315a417b9e3ac89
GabrielCuccia <112596166+GabrielCuccia@users.noreply.github.com>
2022-11-11 15:25:14 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -7,11 +7,11 @@ let peopleValue = parseInt(peopleInput.value)
 let tipAmount = document.querySelector(".results__tip-value")
 let tipTotal = document.querySelector(".results__tip-total")
 let btnValue = 5
+let btnCustom = document.querySelector(".btns__custom")
+let error = document.querySelector("#alert")
 bottons.forEach(element =>{
     element.addEventListener("click", event=>{
-        bottons.forEach(element =>{
-            element.classList.remove("btns__button--selected")
-        })
+        resetBtn()
         element.classList.add("btns__button--selected")
         btnValue = (parseInt(event.target.innerText.slice(0, -1)))
 
@@ -22,15 +22,45 @@ bottons.forEach(element =>{
 })
 billInput.addEventListener("input", ()=>{
     billValue = parseFloat(billInput.value)
-    calculation()
+    if (isNaN(billValue)){
+        billValue = 0
+    }
+    else{
+        calculation()
+    }
+    
 })
 peopleInput.addEventListener("input", ()=>{
     peopleValue = parseFloat(peopleInput.value)
-    calculation()
+    if (peopleValue == 0 || isNaN(peopleValue) ){
+        peopleInput.style.borderColor = "rgb(164, 68, 68)"
+        error.classList.add("alert")
+        reset()
+        
+    }
+    else{
+        calculation()
+        peopleInput.style.borderColor = "var(--Strong-cyan)"
+        error.classList.remove("alert")
+    }
+    
 })
 btnReset.addEventListener("click", ()=>{
-    tipTotal.innerText = 0
-    tipAmount.innerText = 0
+    reset()
+})
+btnCustom.addEventListener("input", ()=>{
+    
+    btnValue = parseFloat(btnCustom.value)
+    if (isNaN(btnValue)) {
+        btnValue = 0
+    }
+    else
+    
+    calculation()
+    
+})
+btnCustom.addEventListener("click", ()=>{
+    resetBtn()
 })
 
 
@@ -40,4 +70,15 @@ function calculation(){
     tipAmount.innerText = ((billValue * btnValue / 100) / peopleValue).toFixed(2)
     tipTotal.innerText = (((billValue * btnValue / 100) + billValue) / peopleValue).toFixed(2)
 }
+function reset(){
+    tipTotal.innerText = 0
+    tipAmount.innerText = 0
+}
+function resetBtn(){
+    bottons.forEach(element =>{
+        element.classList.remove("btns__button--selected")
+        
+    })
+}
+
 
